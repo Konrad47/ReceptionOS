0c0015b4b0502babd2c8a765fa1dafae87f79d55
Konrad47 <konradplichta123@gmail.com>
2025-07-21 10:54:37 +0200

    update/ autopilot section

--- src/views/home/components/Autopilot/styled.components.js
+++ src/views/home/components/Autopilot/styled.components.js
@@ -38,15 +38,61 @@ export const AutopilotComponent = styled.div`
     }
   }
 
+  .phone-wrapper {
+    position: relative;
+    display: inline-block;
+  }
+
+  .tooltip {
+    position: absolute;
+    top: -30px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 178px;
+    box-sizing: border-box;
+    margin: auto;
+    margin-bottom: -30px;
+    opacity: 0;
+    padding: 8px 16px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 16px;
+    border: 1px solid var(--Border, rgba(253, 240, 231, 0.2));
+    background: linear-gradient(
+      99deg,
+      rgba(5, 4, 3, 0.05) 9.46%,
+      rgba(5, 4, 3, 0.3) 90.62%
+    );
+    box-shadow: 2px 2px 8px 0px rgba(255, 255, 255, 0.15) inset;
+    backdrop-filter: blur(5px);
+    transition: all 0.3s ease-in;
+  }
+
   .phone-circle {
+    cursor: pointer;
     width: 315px;
     img {
       width: 315px;
     }
   }
 
+  .phone-wrapper:hover .tooltip {
+    opacity: 1;
+  }
+
+  .data-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+    align-self: stretch;
+    opacity: 0.7;
+  }
+
   @media only screen and (max-width: 1020px) {
     .text-container {
+      padding: 60px 16px 70px 16px;
       h1 {
         width: 100%;
         box-sizing: border-box;
@@ -60,6 +106,10 @@ export const AutopilotComponent = styled.div`
         width: 100%;
         box-sizing: border-box;
       }
+    }
+
+    .tooltip {
+      opacity: 1;
     }
   }
 `
