e3d76bb8fb70780255150e20ad4622c4dae89903
kplichta <konrad.plichta@sim.com.pl>
2025-07-30 13:11:34 +0200

    update/ add trans component

--- src/views/home/components/ClinicsProblems/ClinicsProblems.jsx
+++ src/views/home/components/ClinicsProblems/ClinicsProblems.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { ClinicsProblemsComponent } from "./styled.components"
+import { Trans } from 'react-i18next';
 
 const ClinicsProblems = ({ t }) => {
 
@@ -11,7 +12,14 @@ const ClinicsProblems = ({ t }) => {
             <div className="text-up">
               <h2>{t(`home.ClinicsProblems.title`)}</h2>
             </div>
-            <p className="p-body-large">{t(`home.ClinicsProblems.description`)}</p>
+            <p className="p-body-large">
+              <Trans
+                i18nKey="home.ClinicsProblems.description"
+                components={{
+                  b: <b></b>
+                }}
+              />
+            </p>
           </div>
         </div>
       </ClinicsProblemsComponent>
