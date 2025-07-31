d3c2976384aed2285260b65f9fc0f863be894e43
kplichta <konrad.plichta@sim.com.pl>
2025-07-31 09:28:15 +0200

    update/ footer component

--- src/components/Footer/styled.components.js
+++ src/components/Footer/styled.components.js
@@ -9,6 +9,22 @@ export const FooterComponent = styled.div`
   border: 1px solid var(--Border, rgba(253, 240, 231, 0.2));
   background: #261f17;
 
+  p {
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
+
+  a {
+    color: #f5f5f5;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-decoration: none;
+  }
+
   .footer-1 {
     display: flex;
     align-items: flex-start;
@@ -29,22 +45,6 @@ export const FooterComponent = styled.div`
       justify-content: center;
       align-items: flex-start;
       gap: 16px;
-
-      p {
-        font-size: 16px;
-        font-style: normal;
-        font-weight: 600;
-        line-height: normal;
-      }
-
-      a {
-        color: #f5f5f5;
-        font-size: 16px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: normal;
-        text-decoration: none;
-      }
     }
   }
 
@@ -60,6 +60,34 @@ export const FooterComponent = styled.div`
       display: flex;
       align-items: flex-start;
       gap: 8px;
+
+      a {
+        display: flex;
+        width: 44px;
+        height: 44px;
+        box-sizing: border-box;
+        justify-content: center;
+        align-items: center;
+        border-radius: 16px;
+        border: 1px solid var(--Border, rgba(253, 240, 231, 0.2));
+        background: linear-gradient(
+          99deg,
+          rgba(5, 4, 3, 0.05) 9.46%,
+          rgba(5, 4, 3, 0.3) 90.62%
+        );
+        box-shadow: -2px -2px 8px 0 rgba(255, 255, 255, 0.4) inset;
+        backdrop-filter: blur(5px);
+        transition: 0.3s all ease-in-out;
+
+        &:hover {
+          padding-left: 6px;
+          background: linear-gradient(
+            99deg,
+            rgba(255, 255, 255, 0.05) 9.46%,
+            rgba(255, 255, 255, 0.3) 90.62%
+          );
+        }
+      }
     }
 
     .text {
