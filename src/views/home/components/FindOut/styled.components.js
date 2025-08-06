8f7b85239559c95e976b86589531125db0a0df96
kplichta <konrad.plichta@sim.com.pl>
2025-08-06 17:33:31 +0200

    update/ FindOut component

--- src/views/home/components/FindOut/styled.components.js
+++ src/views/home/components/FindOut/styled.components.js
@@ -12,6 +12,55 @@ export const FindOutComponent = styled.div`
   background-attachment: scroll;
   justify-content: center;
 
+  .container {
+    display: flex;
+    gap: 60px;
+  }
+
+  .left-container {
+    display: flex;
+    max-width: 666px;
+    padding: 80px 0;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 40px;
+
+    .top {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 24px;
+      align-self: stretch;
+    }
+
+    .middle {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 16px;
+      align-self: stretch;
+
+      .point {
+        display: flex;
+        align-items: flex-start;
+        gap: 8px;
+        align-self: stretch;
+
+        svg {
+          width: 24px;
+          height: 24px;
+          flex-shrink: 0;
+        }
+      }
+    }
+
+    .bottom {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+
   @media only screen and (max-width: 1230px) {
     background-image: url(${BackgroundImageMobile});
   }
