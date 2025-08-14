52b500d7187424a16eeb387a82ec1a996b9809e1
kplichta <konrad.plichta@sim.com.pl>
2025-08-14 12:28:29 +0200

    update/ case study component

--- src/views/home/components/CaseStudy/CaseStudy.jsx
+++ src/views/home/components/CaseStudy/CaseStudy.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { CaseStudyComponent } from "./styled.components"
+import { RoundedInfoTile } from "../../../../styled.components"
+import { StaticImage } from "gatsby-plugin-image"
 
 const CaseStudy = ({ t }) => {
 
@@ -7,7 +9,22 @@ const CaseStudy = ({ t }) => {
     <>
       <CaseStudyComponent>
         <div className="container">
-
+          <div className="case-study-top">
+            <RoundedInfoTile>
+              <p>{t('home.CaseStudy.case-study')}</p>
+            </RoundedInfoTile>
+            <h2>{t('home.CaseStudy.title')}</h2>
+            <p className="p-new-model-16">
+              {t('home.CaseStudy.description')}
+            </p>
+          </div>
+          <StaticImage
+            className="apolonia-cs"
+            src="../../../../images/case-study/apolonia-logo.webp"
+            alt="apolonia"
+            placeholder="apolonia"
+            loading="lazy"
+          />
         </div>
       </CaseStudyComponent>
     </>
--- src/views/home/components/CaseStudy/styled.components.js
+++ src/views/home/components/CaseStudy/styled.components.js
@@ -1,3 +1,32 @@
 import styled from "styled-components"
 
-export const CaseStudyComponent = styled.div``
+export const CaseStudyComponent = styled.div`
+  padding: 30px 0;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+  }
+
+  .case-study-top {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+    max-width: 960px;
+    text-align: center;
+
+    p {
+      max-width: 614px;
+    }
+  }
+  .apolonia-cs {
+    width: 118px;
+    img {
+      width: 118px;
+    }
+  }
+`
