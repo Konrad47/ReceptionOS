93d3a32826a8bca59645e08f777722f749f60626
kplichta <konrad.plichta@sim.com.pl>
2025-08-17 17:15:24 +0200

    update/ BorderContainerTopRowsSides component

--- src/components/BorderContainer/BorderContainerTopRowsSides.jsx
+++ src/components/BorderContainer/BorderContainerTopRowsSides.jsx
@@ -24,23 +24,87 @@ export const BorderContainerTopRowsSides = ({ children, className }) => {
                 >
                     <path d="M1 1L7 1L7 7" stroke="#877B72" stroke-linecap="round" />
                 </svg>
-                <svg className="left" xmlns="http://www.w3.org/2000/svg" width="34" height="833" viewBox="0 0 34 833" fill="none">
-                    <path d="M33 0.5L33 105.5L27 136.5L33 136.5L1 245.75L1 832.5" stroke="url(#paint0_linear_206_3252)" stroke-linecap="round" />
+                <svg
+                    className="left desktop"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="34"
+                    height="833"
+                    viewBox="0 0 34 833"
+                    fill="none"
+                >
+                    <path
+                        d="M33 0.5L33 105.5L27 136.5L33 136.5L1 245.75L1 832.5"
+                        stroke="url(#paint0_linear_206_3252)"
+                        stroke-linecap="round"
+                    />
                     <defs>
-                        <linearGradient id="paint0_linear_206_3252" x1="17" y1="358" x2="17" y2="833" gradientUnits="userSpaceOnUse">
+                        <linearGradient
+                            id="paint0_linear_206_3252"
+                            x1="17"
+                            y1="358"
+                            x2="17"
+                            y2="833"
+                            gradientUnits="userSpaceOnUse"
+                        >
                             <stop stop-color="#302C29" />
                             <stop offset="1" stop-color="#302C29" stop-opacity="0" />
                         </linearGradient>
                     </defs>
                 </svg>
-                <svg className="right" xmlns="http://www.w3.org/2000/svg" width="34" height="792" viewBox="0 0 34 792" fill="none">
-                    <path d="M1 0.5L0.999997 105.5L7 136.5L0.999995 136.5L33 245.75L33 791" stroke="url(#paint0_linear_206_3251)" stroke-linecap="round" />
+                <svg
+                    className="left mobile"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="34"
+                    height="431"
+                    viewBox="0 0 34 431"
+                    fill="none"
+                >
+                    <path
+                        d="M33 -4L33 101L27 132L33 132L1 241.25L1 430.5"
+                        stroke="#302C29"
+                        stroke-linecap="round"
+                    />
+                </svg>
+                <svg
+                    className="right desktop"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="34"
+                    height="792"
+                    viewBox="0 0 34 792"
+                    fill="none"
+                >
+                    <path
+                        d="M1 0.5L0.999997 105.5L7 136.5L0.999995 136.5L33 245.75L33 791"
+                        stroke="url(#paint0_linear_206_3251)"
+                        stroke-linecap="round"
+                    />
                     <defs>
-                        <linearGradient id="paint0_linear_206_3251" x1="17" y1="440.5" x2="16.9994" y2="820" gradientUnits="userSpaceOnUse">
+                        <linearGradient
+                            id="paint0_linear_206_3251"
+                            x1="17"
+                            y1="440.5"
+                            x2="16.9994"
+                            y2="820"
+                            gradientUnits="userSpaceOnUse"
+                        >
                             <stop stop-color="#302C29" />
                             <stop offset="1" stop-color="#302C29" stop-opacity="0" />
                         </linearGradient>
                     </defs>
+                </svg>
+                <svg
+                    className="right mobile"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="34"
+                    height="431"
+                    viewBox="0 0 34 431"
+                    fill="none"
+                >
+                    <path
+                        d="M1 -4L0.999997 101L7 132L0.999995 132L33 241.25L33 430.5"
+                        stroke="#302C29"
+                        stroke-linecap="round"
+                    />
                 </svg>
                 {children}
                 <style jsx>{`
@@ -73,30 +137,6 @@ export const BorderContainerTopRowsSides = ({ children, className }) => {
           }
         `}</style>
             </div>
-            {/* <svg
-                className="bottom desktop"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 963 111"
-                fill="none"
-            >
-                <path
-                    d="M962 0.5V94L933 104H875.5L844.5 110V104L30 104.134L1 94.134V0.5"
-                    stroke="#302C29"
-                    stroke-linecap="round"
-                />
-            </svg>
-            <svg
-                className="bottom mobile"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 360 103"
-                fill="none"
-            >
-                <path
-                    d="M359 1L359 86L331 95.9999H273.5L242.5 102V95.9999H30L0.999969 86V1"
-                    stroke="#302C29"
-                    stroke-linecap="round"
-                />
-            </svg> */}
         </>
     )
 }
