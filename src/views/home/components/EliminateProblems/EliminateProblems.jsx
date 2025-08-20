ba233648f5199ba14268f64c82e6c8e62dae4871
kplichta <konrad.plichta@sim.com.pl>
2025-08-20 15:02:06 +0200

    update/ EliminateProblems component

--- src/views/home/components/EliminateProblems/EliminateProblems.jsx
+++ src/views/home/components/EliminateProblems/EliminateProblems.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { EliminateProblemsComponent } from "./styled.components"
-import { RoundedInfoTile } from "../../../../styled.components"
+import { RoundedButtonSvg, RoundedInfoTile } from "../../../../styled.components"
+import { BorderContainer4Rows } from "../../../../components/BorderContainer/BorderContainer4Rows"
 
 const EliminateProblems = ({ t }) => {
 
@@ -13,7 +14,26 @@ const EliminateProblems = ({ t }) => {
               <p>{t('home.EliminateProblems.eliminate-problems')}</p>
             </RoundedInfoTile>
             <h3>{t('home.EliminateProblems.title')}</h3>
-            <p className="p-new-model-18 ">{t('home.EliminateProblems.description')}</p>
+            <p className="p-new-model-18">{t('home.EliminateProblems.description')}</p>
+          </div>
+          <div className="tiles-container">
+            {['1', '2', '3']
+              .map(key => (
+                <BorderContainer4Rows key={key} className="tile-container">
+                  <div className="tile-left">
+                    <p className="p-new-model-18">{t(`home.EliminateProblems.tile${key}-title`)}</p>
+                    <p className="p-new-model-16">{t(`home.EliminateProblems.tile${key}-description`)}</p>
+                  </div>
+                  <div className="tile-right">
+                    <RoundedButtonSvg>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <path d="M2.92871 9.99995H17.0716M17.0716 9.99995L11.5716 4.5M17.0716 9.99995L11.5716 15.4999" stroke="#FFE8D9" stroke-linejoin="round" />
+                      </svg>
+                    </RoundedButtonSvg>
+                  </div>
+                </BorderContainer4Rows>
+              ))
+            }
           </div>
         </EliminateProblemsComponent>
       </div>
--- src/views/home/components/EliminateProblems/styled.components.js
+++ src/views/home/components/EliminateProblems/styled.components.js
@@ -12,4 +12,39 @@ export const EliminateProblemsComponent = styled.div`
     gap: 16px;
     max-width: 798px;
   }
+
+  .tiles-container {
+    width: 100%;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+  }
+
+  .tile-container {
+    display: flex;
+    align-items: flex-start;
+    align-self: stretch;
+    gap: 24px;
+    flex: 1 0 0;
+    padding: 24px;
+    height: 255px;
+    box-sizing: border-box;
+
+    .tile-left {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      gap: 8px;
+      flex: 1 0 0;
+    }
+
+    .tile-right {
+      display: flex;
+      justify-content: flex-end;
+      align-items: flex-end;
+      gap: 8px;
+      align-self: stretch;
+    }
+  }
 `
