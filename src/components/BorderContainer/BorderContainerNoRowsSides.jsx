52e75363ccf29937178ce6fb30dd1a4b9e819a9c
kplichta <konrad.plichta@sim.com.pl>
2025-08-24 12:17:48 +0200

    update/ how it work component

--- src/components/BorderContainer/BorderContainerNoRowsSides.jsx
+++ src/components/BorderContainer/BorderContainerNoRowsSides.jsx
@@ -7,40 +7,13 @@ export const BorderContainerNoRowsSides = ({ children, className }) => {
                 <svg
                     className="left desktop"
                     xmlns="http://www.w3.org/2000/svg"
-                    width="34"
-                    height="833"
-                    viewBox="0 0 34 833"
+                    width="31"
+                    height="248"
+                    viewBox="0 0 31 248"
                     fill="none"
                 >
                     <path
-                        d="M33 0.5L33 105.5L27 136.5L33 136.5L1 245.75L1 832.5"
-                        stroke="url(#paint0_linear_206_3252)"
-                        stroke-linecap="round"
-                    />
-                    <defs>
-                        <linearGradient
-                            id="paint0_linear_206_3252"
-                            x1="17"
-                            y1="358"
-                            x2="17"
-                            y2="833"
-                            gradientUnits="userSpaceOnUse"
-                        >
-                            <stop stop-color="#302C29" />
-                            <stop offset="1" stop-color="#302C29" stop-opacity="0" />
-                        </linearGradient>
-                    </defs>
-                </svg>
-                <svg
-                    className="left mobile"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="34"
-                    height="431"
-                    viewBox="0 0 34 431"
-                    fill="none"
-                >
-                    <path
-                        d="M33 -4L33 101L27 132L33 132L1 241.25L1 430.5"
+                        d="M30 1L29.9998 56L23.9998 87L29.9998 87L0.999999 196.25L0.999999 247"
                         stroke="#302C29"
                         stroke-linecap="round"
                     />
@@ -48,40 +21,13 @@ export const BorderContainerNoRowsSides = ({ children, className }) => {
                 <svg
                     className="right desktop"
                     xmlns="http://www.w3.org/2000/svg"
-                    width="34"
-                    height="792"
-                    viewBox="0 0 34 792"
+                    width="31"
+                    height="248"
+                    viewBox="0 0 31 248"
                     fill="none"
                 >
                     <path
-                        d="M1 0.5L0.999997 105.5L7 136.5L0.999995 136.5L33 245.75L33 791"
-                        stroke="url(#paint0_linear_206_3251)"
-                        stroke-linecap="round"
-                    />
-                    <defs>
-                        <linearGradient
-                            id="paint0_linear_206_3251"
-                            x1="17"
-                            y1="440.5"
-                            x2="16.9994"
-                            y2="820"
-                            gradientUnits="userSpaceOnUse"
-                        >
-                            <stop stop-color="#302C29" />
-                            <stop offset="1" stop-color="#302C29" stop-opacity="0" />
-                        </linearGradient>
-                    </defs>
-                </svg>
-                <svg
-                    className="right mobile"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="34"
-                    height="431"
-                    viewBox="0 0 34 431"
-                    fill="none"
-                >
-                    <path
-                        d="M1 -4L0.999997 101L7 132L0.999995 132L33 241.25L33 430.5"
+                        d="M1 1L1.00024 56L7.00024 87L1.00024 87L30 196.25L30 247"
                         stroke="#302C29"
                         stroke-linecap="round"
                     />
@@ -89,7 +35,6 @@ export const BorderContainerNoRowsSides = ({ children, className }) => {
                 {children}
                 <style jsx>{`
           .border-container-no-rows-sides {
-            /* border-top: 1px solid #302c29; */
             display: flex;
             flex-direction: column;
             position: relative;
--- src/views/home/components/HowItWorks/styled.components.js
+++ src/views/home/components/HowItWorks/styled.components.js
@@ -1,16 +1,17 @@
 import styled from "styled-components"
 import BackgroundImage from "../../../../images/how/how-background.webp"
+import BackgroundImageMobile from "../../../../images/how/how-background_mobile.webp"
 
 export const HowItWorksComponent = styled.div`
   overflow: hidden;
-  background-size: cover !important;
-  background-position: bottom;
+  background-size: contain !important;
+  background-repeat: no-repeat;
+  background-position: top;
   width: 100%;
   background-image: url(${BackgroundImage});
   display: block;
   background-attachment: scroll;
   justify-content: center;
-  height: 246px;
 
   .top-tile {
     display: flex;
@@ -20,5 +21,12 @@ export const HowItWorksComponent = styled.div`
     align-items: center;
     text-align: center;
     gap: 16px;
+  }
+
+  @media only screen and (max-width: 1020px) {
+    background-image: url(${BackgroundImageMobile});
+    .top-tile {
+      margin: 44px auto;
+    }
   }
 `
