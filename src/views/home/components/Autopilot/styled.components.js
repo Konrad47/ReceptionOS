ef7fd50e706874f74e4b73fe0364431b71cddb21
kplichta <konrad.plichta@sim.com.pl>
2025-08-27 09:00:27 +0200

    update/ autopilot

--- src/views/home/components/Autopilot/styled.components.js
+++ src/views/home/components/Autopilot/styled.components.js
@@ -30,44 +30,12 @@ export const AutopilotComponent = styled.div`
     }
 
     span {
-      color: var(--Orange);
-      /* padding: 0 115px; */
+      padding: 0 135px;
     }
 
     p {
-      width: 700px;
+      width: 411px;
     }
-  }
-
-  .phone-wrapper {
-    position: relative;
-    display: inline-block;
-  }
-
-  .tooltip {
-    position: absolute;
-    top: -30px;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 178px;
-    box-sizing: border-box;
-    margin: auto;
-    margin-bottom: -30px;
-    opacity: 0;
-    padding: 8px 16px;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-    border-radius: 16px;
-    border: 1px solid var(--Border, rgba(253, 240, 231, 0.2));
-    background: linear-gradient(
-      99deg,
-      rgba(5, 4, 3, 0.05) 9.46%,
-      rgba(5, 4, 3, 0.3) 90.62%
-    );
-    box-shadow: 2px 2px 8px 0px rgba(255, 255, 255, 0.15) inset;
-    backdrop-filter: blur(5px);
-    transition: all 0.3s ease-in;
   }
 
   .phone-circle {
@@ -76,10 +44,6 @@ export const AutopilotComponent = styled.div`
     img {
       width: 315px;
     }
-  }
-
-  .phone-wrapper:hover .tooltip {
-    opacity: 1;
   }
 
   .data-info {
